4a7ea01f76ce936e9df9bb9eeffcbe98531ac4d8
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2023-06-22 20:01:11 -0300

    feat: add freetogame url in Game Card

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -19,7 +19,7 @@ export function GameCard({
   return (
     <Link href={game_url} target="_blank">
       <div className="max-w-[400px] min-w-[200px] h-96 flex flex-col">
-        <h3 className="text-2xl mb-1">{title}</h3>
+        <h3 className="text-2xl mb-1 font-bold">{title}</h3>
         <div className="relative flex-1 overflow-hidden rounded-lg">
           <Image
             src={thumbnail}
@@ -28,15 +28,32 @@ export function GameCard({
             className="object-cover"
           />
         </div>
-        <div className="mt-1">
-          <span className="text-blue-600">{publisher}</span>
-          <p className="text-xs">{short_description}</p>
-          <span className="text-rose-600 text-sm">{genre}</span>
-          <div className="flex flex-row justify-between">
-            <span>{developer}</span>
-            <span className="text-xs">{release_date}</span>
+        <div className="mt-1 flex flex-col items-start gap-1">
+          <span className="text-sm">
+            Publisher: {` `}
+            <span className="text-blue-600 font-bold">{publisher}</span>
+          </span>
+          <span className="text-sm">
+            Developer:{" "}
+            <span className="text-purple-600 font-bold">{developer}</span>
+          </span>
+          <p className="text-xs text-gray-400">{short_description}</p>
+          <span className="bg-rose-600 text-sm text-rose-100 flex-0 px-1 rounded font-bold">
+            {genre}
+          </span>
+          <div className="flex flex-row w-full justify-between">
+            <span className="text-xs">Platform: {platform}</span>
+            <span className="text-xs text-gray-300">
+              Release: {release_date}
+            </span>
           </div>
-          <span>Platform: {platform}</span>
+          <Link
+            href={freetogame_profile_url}
+            target="_blank"
+            className="text-blue-500 text-sm font-bold underline"
+          >
+            Open freetogame
+          </Link>
         </div>
       </div>
     </Link>

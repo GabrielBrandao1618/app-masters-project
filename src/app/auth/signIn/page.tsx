56ad34cbded44e3cb3fcb7b4f9f535b7dbd17d60
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2023-07-07 10:52:41 -0300

    feat: create sign-in page

---
+++ src/app/auth/signIn/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { FormEvent, useRef } from "react";
+
+export default function SignInPage() {
+  async function onFormSubmit(e: FormEvent) {
+    e.preventDefault();
+  }
+  return (
+    <main className="flex items-center justify-center flex-col">
+      <form onSubmit={onFormSubmit} className="px-2 py-10 w-full max-w-md">
+        <h1 className="text-5xl font-bold w-full text-center mb-12">
+          Sign <span className="text-blue-600">in</span>
+        </h1>
+        <div className="flex flex-col gap-2 w-full">
+          <input
+            type="text"
+            placeholder="youremail@gmail.com"
+            className="bg-gray-800 px-2 py-1 rounded w-full"
+            required
+          />
+          <input
+            type="text"
+            placeholder="your password"
+            className="bg-gray-800 px-2 py-1 rounded w-full"
+            required
+          />
+          <button
+            type="submit"
+            className="bg-blue-600 px-3 py-2 rounded hover:bg-blue-700 transition-all font-bold"
+          >
+            Sign in
+          </button>
+        </div>
+      </form>
+    </main>
+  );
+}

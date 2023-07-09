10453ccb03225e9e624bd82d24f854011691b25d
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2023-07-09 14:53:03 -0300

    feat: add links between sign-in and sign-up page

--- src/app/auth/signIn/page.tsx
+++ src/app/auth/signIn/page.tsx
@@ -7,6 +7,8 @@ import { useAuth } from "@/contexts/AuthContext";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { FirebaseError } from "firebase/app";
+import { Header } from "@/components/Header";
+import Link from "next/link";
 
 export default function SignInPage() {
   const { replace } = useRouter();
@@ -27,37 +29,49 @@ export default function SignInPage() {
     replace("/");
   }
   return (
-    <main className="flex items-center justify-center flex-col">
-      <form
-        onSubmit={handleSubmit(onFormSubmit)}
-        className="px-2 py-10 w-full max-w-md"
-      >
-        <h1 className="text-5xl font-bold w-full text-center mb-12">
-          Sign <span className="text-blue-600">in</span>
-        </h1>
-        <div className="flex flex-col gap-2 w-full">
-          <input
-            type="text"
-            placeholder="youremail@gmail.com"
-            className="bg-transparent border-gray-800 border px-2 py-1 rounded w-full"
-            {...register("email")}
-          />
-          <input
-            type="password"
-            placeholder="your password"
-            className="bg-transparent border-gray-800 border px-2 py-1 rounded w-full"
-            {...register("password")}
-          />
-          <button
-            type="submit"
-            className="bg-blue-600 px-3 py-2 rounded hover:bg-blue-700 transition-all font-bold"
-          >
-            Sign in
-          </button>
-          <span>{errorLabelText}</span>
-        </div>
-      </form>
-    </main>
+    <>
+      <Header />
+      <main className="flex items-center justify-center flex-col py-16">
+        <form
+          onSubmit={handleSubmit(onFormSubmit)}
+          className="px-2 py-10 w-full max-w-md"
+        >
+          <h1 className="text-5xl font-bold w-full text-center mb-2">
+            Sign <span className="text-blue-600">in</span>
+          </h1>
+          <h3 className="mb-12 w-full text-center text-gray-400">
+            App masters game store
+          </h3>
+          <div className="flex flex-col gap-2 w-full">
+            <input
+              type="text"
+              placeholder="youremail@gmail.com"
+              className="bg-transparent border-gray-800 border px-3 py-2 rounded w-full"
+              {...register("email")}
+            />
+            <input
+              type="password"
+              placeholder="your password"
+              className="bg-transparent border-gray-800 border px-3 py-2 rounded w-full"
+              {...register("password")}
+            />
+            <button
+              type="submit"
+              className="bg-blue-600 px-3 py-2 rounded hover:bg-blue-700 transition-all font-bold"
+            >
+              Sign in
+            </button>
+            <span>
+              Don't have an account?{" "}
+              <Link href="/auth/signUp" className="text-blue-400 font-bold">
+                sign-up here
+              </Link>
+            </span>
+            <span>{errorLabelText}</span>
+          </div>
+        </form>
+      </main>
+    </>
   );
 }
 
--- src/app/auth/signUp/page.tsx
+++ src/app/auth/signUp/page.tsx
@@ -7,6 +7,8 @@ import { useAuth } from "@/contexts/AuthContext";
 import { useRouter } from "next/navigation";
 import { FirebaseError } from "firebase/app";
 import { useState } from "react";
+import { Header } from "@/components/Header";
+import Link from "next/link";
 
 export default function SignUpPage() {
   const { replace } = useRouter();
@@ -27,43 +29,55 @@ export default function SignUpPage() {
     replace("/");
   }
   return (
-    <main className="flex items-center justify-center flex-col">
-      <form
-        onSubmit={handleSubmit(onFormSubmit)}
-        className="px-2 py-10 w-full max-w-md"
-      >
-        <h1 className="text-5xl font-bold w-full text-center mb-12">
-          Sign <span className="text-blue-600">up</span>
-        </h1>
-        <div className="flex flex-col gap-2 w-full">
-          <input
-            type="text"
-            placeholder="youremail@gmail.com"
-            className="bg-transparent border-gray-800 border px-2 py-1 rounded w-full"
-            {...register("email")}
-          />
-          <input
-            type="password"
-            placeholder="your password"
-            className="bg-transparent border-gray-800 border px-2 py-1 rounded w-full"
-            {...register("password")}
-          />
-          <input
-            type="password"
-            placeholder="confirm your password"
-            className="bg-transparent border-gray-800 border px-2 py-1 rounded w-full"
-            {...register("confirmPassword")}
-          />
-          <button
-            type="submit"
-            className="bg-blue-600 px-3 py-2 rounded hover:bg-blue-700 transition-all font-bold"
-          >
-            Sign in
-          </button>
-          <span>{errorLabelText}</span>
-        </div>
-      </form>
-    </main>
+    <>
+      <Header />
+      <main className="flex items-center justify-center flex-col py-16">
+        <form
+          onSubmit={handleSubmit(onFormSubmit)}
+          className="px-2 py-10 w-full max-w-md"
+        >
+          <h1 className="text-5xl font-bold w-full text-center mb-2">
+            Sign <span className="text-blue-600">up</span>
+          </h1>
+          <h3 className="mb-12 w-full text-center text-gray-400">
+            App masters game store
+          </h3>
+          <div className="flex flex-col gap-2 w-full">
+            <input
+              type="text"
+              placeholder="youremail@gmail.com"
+              className="bg-transparent border-gray-800 border px-3 py-2 rounded w-full"
+              {...register("email")}
+            />
+            <input
+              type="password"
+              placeholder="your password"
+              className="bg-transparent border-gray-800 border px-3 py-2 rounded w-full"
+              {...register("password")}
+            />
+            <input
+              type="password"
+              placeholder="confirm your password"
+              className="bg-transparent border-gray-800 border px-3 py-2 rounded w-full"
+              {...register("confirmPassword")}
+            />
+            <button
+              type="submit"
+              className="bg-blue-600 px-3 py-2 rounded hover:bg-blue-700 transition-all font-bold"
+            >
+              Sign in
+            </button>
+            <span>
+              Already have an account?{" "}
+              <Link href="/auth/signIn" className="text-blue-400 font-bold">
+                sign-in here
+              </Link>
+            </span>
+            <span>{errorLabelText}</span>
+          </div>
+        </form>
+      </main>
+    </>
   );
 }
 
